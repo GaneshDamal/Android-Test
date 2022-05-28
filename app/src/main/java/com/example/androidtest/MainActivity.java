d9c4331828893a5153f93c8ef406c035092cf2b0
Ganesh <damalganesh47@gmail.com>
2022-05-28 11:45:01 +0530

    Android-Test

---
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String url = "https://api.androidhive.info/contacts/";
+
+
+    }
+}
